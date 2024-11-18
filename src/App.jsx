2f6881ed198992156d70d985a054e2a18aa1b61d
unknown <ahmadfuadoorakam@gmail.com>
2024-11-18 08:55:31 +0530

    added Result route in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import './index.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home/Home";
+import Result from "./Pages/Result/Result";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/result' element={<Result />} />
         </Routes>
       </BrowserRouter>
     </>
